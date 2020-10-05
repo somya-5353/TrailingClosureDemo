a92f6ad60b107f12ac23004e14c4d0ba7cd0c6d7
Somya <somyacarmel5353@gmail.com>
2020-10-05 22:48:51 +0530

    initial commit

---
+++ TrailingClosureDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrailingClosureDemo
+//
+//  Created by Somya on 05/10/20.
+//  Copyright © 2020 Somya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
